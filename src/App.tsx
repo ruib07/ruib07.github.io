a224bc7c53fbd19c98f3f32b147b3da4af6fd5ed
Rui Barreto <ruibarreto07@outlook.com>
2025-02-02 13:46:56 +0000

    Folder structure changed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
-import ScrollToTop from "./hooks/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
