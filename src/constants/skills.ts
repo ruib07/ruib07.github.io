e87f22899f34a032d48fa4ee7797c1acfe0b4906
Rui Barreto <ruibarreto07@outlook.com>
2025-04-01 18:04:33 +0100

    Skills added.

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,9 +1,12 @@
 import { AiOutlineDotNet } from "react-icons/ai";
+import { BiLogoPostgresql } from "react-icons/bi";
 import { DiMsqlServer } from "react-icons/di";
-import { FaReact } from "react-icons/fa6";
+import { FaNodeJs, FaReact } from "react-icons/fa6";
 
 export const skillsData = [
   { src: FaReact },
   { src: AiOutlineDotNet },
+  { src: FaNodeJs },
   { src: DiMsqlServer },
+  { src: BiLogoPostgresql },
 ];
