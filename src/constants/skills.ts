ee01b2db29f0eaf5117e509fab9f20270ec9b0ac
Rui Barreto <ruibarreto07@outlook.com>
2025-08-02 22:36:47 +0100

    Actual experience description updated and skills about me updated.

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -1,14 +1,11 @@
 import { AiOutlineDotNet } from "react-icons/ai";
-import { BiLogoPostgresql } from "react-icons/bi";
 import { DiMsqlServer } from "react-icons/di";
-import { FaNodeJs, FaReact } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 
 export const skillsData = [
   { src: FaReact },
   { src: RiTailwindCssFill },
   { src: AiOutlineDotNet },
-  { src: FaNodeJs },
   { src: DiMsqlServer },
-  { src: BiLogoPostgresql },
 ];
